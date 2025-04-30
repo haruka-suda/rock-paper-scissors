7beab0b7706f44d372b8f695ee6a3cfc00a1292f
Haruka Suda <209753347+haruka-suda@users.noreply.github.com>
2025-04-30 18:57:59 +0900

    Initial Commit

---
+++ rock-paper-scissors/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  rock-paper-scissors
+//
+//  Created by Mac mini on 2025/04/30.
+//
+
+// testing for Github
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rock-paper-scissors/rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rock_paper_scissorsApp.swift
+//  rock-paper-scissors
+//
+//  Created by Mac mini on 2025/04/30.
+//
+
+import SwiftUI
+
+@main
+struct rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
