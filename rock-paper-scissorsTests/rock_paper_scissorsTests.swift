c81c4e0f476916e02039f25c8e8b267cdf6fbe0e
Haruka Suda <209753347+haruka-suda@users.noreply.github.com>
2025-04-30 18:57:59 +0900

    Initial Commit

---
+++ rock-paper-scissorsTests/rock_paper_scissorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  rock_paper_scissorsTests.swift
+//  rock-paper-scissorsTests
+//
+//  Created by Mac mini on 2025/04/30.
+//
+
+import Testing
+@testable import rock_paper_scissors
+
+struct rock_paper_scissorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
