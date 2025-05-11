bd088c3260d22ce9079793ec1c9f81bfcefdf27b
Haruka Suda <209753347+haruka-suda@users.noreply.github.com>
2025-05-11 16:54:24 +0900

    wip: separate variables, enums, RPS logic, and sound manager

---
+++ rock-paper-scissors/NormalModeView.swift
@@ -0,0 +1,18 @@
+//
+//  NormalModeView.swift
+//  rock-paper-scissors
+//
+//  Created by Mac mini on 2025/05/11.
+//
+
+import SwiftUI
+
+struct NormalModeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NormalModeView()
+}
