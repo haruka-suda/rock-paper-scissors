8d893c5243cceb6c75b5b0f597345b5c5c50bfc0
Haruka Suda <209753347+haruka-suda@users.noreply.github.com>
2025-05-13 14:09:22 +0900

    refactor: split modes into separate views

---
+++ rock-paper-scissors/BrainTrainingView.swift
@@ -0,0 +1,18 @@
+//
+//  BrainTrainingView.swift
+//  rock-paper-scissors
+//
+//  Created by Mac mini on 2025/05/13.
+//
+
+import SwiftUI
+
+struct BrainTrainingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrainTrainingView()
+}
