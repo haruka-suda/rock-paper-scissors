85f466b5b120ca4da5e2df50a9e1e7fcd9baed53
Haruka Suda <209753347+haruka-suda@users.noreply.github.com>
2025-05-15 14:06:43 +0900

    refactor: separate FinishedView and ResultView into individual files

---
+++ rock-paper-scissors/FinishedView.swift
@@ -0,0 +1,18 @@
+//
+//  FinishedView.swift
+//  rock-paper-scissors
+//
+//  Created by Mac mini on 2025/05/14.
+//
+
+import SwiftUI
+
+struct FinishedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinishedView()
+}
