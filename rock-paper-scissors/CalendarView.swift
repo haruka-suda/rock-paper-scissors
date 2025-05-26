788d2e735bb8b938b393e75c7246cbbbb049bc18
Haruka Suda <209753347+haruka-suda@users.noreply.github.com>
2025-05-26 21:49:09 +0900

    wip: create a calendar framework

---
+++ rock-paper-scissors/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  rock-paper-scissors
+//
+//  Created by Mac mini on 2025/05/24.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
